9c705d39b54774b23749f9a141eedd04aed9d202
shankslevi <166959357+shankslevi@users.noreply.github.com>
2025-05-20 21:00:28 -0300

    New db

--- main.tf
+++ main.tf
@@ -13,6 +13,21 @@ module "kms" {
 
 module "rds" {
   source               = "./modules/rds"
+  db_identifier        = "fastfood-db"
+  db_instance_class    = "db.t3.micro"
+  db_allocated_storage = 5
+  db_max_allocated_storage = 10
+  db_name              = "fastfood"
+  db_username          = "postgres"
+  db_password          = var.db_password
+  security_group_ids   = [module.security_groups.rds_sg_id]
+  subnet_ids           = module.vpc.private_subnets
+  environment          = var.environment
+  kms_key_id           = module.kms.key_id
+}
+
+module "rds" {
+  source               = "./modules/rds"
   db_identifier        = "fastfood-payments-db"
   db_instance_class    = "db.t3.micro"
   db_allocated_storage = 5
