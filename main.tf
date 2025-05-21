c84ac5585b5cf687125999faf0e3e3415bf99471
Samuel Levi <samuellevi.job@gmail.com>
2025-05-20 21:06:13 -0300

    removing db

--- main.tf
+++ main.tf
@@ -11,21 +11,6 @@ module "kms" {
   source = "./modules/kms"
 }
 
-module "rds_fastfood" {
-  source               = "./modules/rds"
-  db_identifier        = "fastfood-db"
-  db_instance_class    = "db.t3.micro"
-  db_allocated_storage = 5
-  db_max_allocated_storage = 10
-  db_name              = "fastfood"
-  db_username          = "postgres"
-  db_password          = var.db_password
-  security_group_ids   = [module.security_groups.rds_sg_id]
-  subnet_ids           = module.vpc.private_subnets
-  environment          = var.environment
-  kms_key_id           = module.kms.key_id
-}
-
 module "rds" {
   source               = "./modules/rds"
   db_identifier        = "fastfood-payments-db"
